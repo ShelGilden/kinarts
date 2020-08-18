2465727a0bd242439cb2ce537f8271d45b7dfed9
EfraimKrug <efraimmkrug@gmail.com>
2020-08-17 23:10:57 -0400

    Updates

---
+++ precache-manifest.e73e61e575c2bb87de6842bd345b2572.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b79dcf4ca604ba459f88b9d389a48c47",
+    "url": "/kinarts/index.html"
+  },
+  {
+    "revision": "113262c8575c6f92a7ef",
+    "url": "/kinarts/static/css/main.7fff2867.chunk.css"
+  },
+  {
+    "revision": "0c914bfaca9bc5cbd3f9",
+    "url": "/kinarts/static/js/2.5e859bb3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/kinarts/static/js/2.5e859bb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "113262c8575c6f92a7ef",
+    "url": "/kinarts/static/js/main.4759b931.chunk.js"
+  },
+  {
+    "revision": "592108d7cfd9e46e7cb7",
+    "url": "/kinarts/static/js/runtime-main.b7138c68.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/kinarts/static/media/logo.5d5d9eef.svg"
+  }
+]);
